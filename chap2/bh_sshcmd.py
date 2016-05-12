30e6707e92699633f048d7171b8796914deabd01
oga <oga.ivc.s27@gmail.com>
2016-05-12 23:34:46 +0900

    make directory

---
+++ chap2/bh_sshcmd.py
@@ -0,0 +1,16 @@
+import paramiko
+
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    # client.load_host_keys('/home/jackdaw/.ssh/known_hosts')
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024).decode())
+
+
+if __name__ == "__main__":
+    ssh_command("localhost", "jackdaw", "blackhatpython", 'id')
